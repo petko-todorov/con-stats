6ba0b2bdcc92a424951a112da05b4060fb952094
Petko Todorov <p.todorov1015@gmail.com>
2025-02-21 11:41:39 +0200

    made scrape all entered games and fix small bugs

--- scraper.py
+++ scraper.py
@@ -3,16 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC  # NOQA
 from dotenv import dotenv_values
-import os
 import time
 
 CONFIG = dotenv_values()
 
 
 def scrape_data():
+    stats = {}
+
     username = CONFIG['USERNAME']
     password = CONFIG['PASSWORD']
 
@@ -44,13 +44,6 @@ def scrape_data():
         iframe = driver.find_element(By.ID, "ifm")
         driver.switch_to.frame(iframe)
 
-        try:
-            WebDriverWait(driver, 20).until(
-                EC.visibility_of_element_located((By.ID, "popupsContainer"))
-            )
-        except TimeoutException:
-            pass
-
         popup_container = driver.find_element(By.ID, "popupsContainer")
         if popup_container:
             driver.execute_script("""
@@ -60,61 +53,91 @@ def scrape_data():
                         }
                     """)
 
-        enter_game = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "#uber_my_games > div:nth-child(1) > div > div"))
+        elements = driver.find_elements(
+            By.XPATH,
+            "//div[contains(@class, 'v-popover') and contains(@class, 'uber_game_tile') and contains(@class, 'button_click')]"
         )
-        enter_game.click()
 
-        # time.sleep(10)
-
-        driver.switch_to.default_content()
-
-        iframe = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.ID, "ifm"))
-        )
-        driver.switch_to.frame(iframe)
-
-        stats = {}
-        nation = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((
-                By.CSS_SELECTOR,
-                "#func_miniprofile_cont > div.miniprofile_name_container.func_open_profile_tooltip > div > "
-                "div:nth-child(2)"))
-        ).text
-        stats[nation] = []
-
-        diplomacy = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.ID, "func_btn_newspaper"))
-        )
-        diplomacy.click()
-
-        victory_points = []
-        while True:
-            time.sleep(0.2)
-
-            ranking_points_element = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located(
-                    (By.CSS_SELECTOR,
-                     "#newspaper_ranking_single > div.viewport > div > ol > li.is_self > div.ranking_points"))
+        def get_data():
+            driver.switch_to.default_content()
+            iframe = WebDriverWait(driver, 20).until(  # NOQA
+                EC.presence_of_element_located((By.ID, "ifm"))
             )
+            driver.switch_to.frame(iframe)
 
-            inner_html = int(ranking_points_element.get_attribute("innerText").strip())
+            nation = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((
+                    By.CSS_SELECTOR,
+                    "#func_miniprofile_cont > div.miniprofile_name_container.func_open_profile_tooltip > div > "
+                    "div:nth-child(2)"))
+            ).text
+            stats[nation] = []
 
-            victory_points.append(inner_html)
-            button = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                            "#func_day_down"))
+            diplomacy = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.ID, "func_btn_newspaper"))
             )
-            button.click()
+            diplomacy.click()
 
-            current_day_element = driver.find_element(By.CSS_SELECTOR, "#func_newspaper_day_tf")
-            current_day_value = current_day_element.get_attribute("value")
+            victory_points = []
+            while True:
+                time.sleep(0.2)
 
-            if current_day_value == "1":
-                break
+                ranking_points_element = WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located(
+                        (By.CSS_SELECTOR,
+                         "#newspaper_ranking_single > div.viewport > div > ol > li.is_self > div.ranking_points"))
+                )
 
-        victory_points.reverse()
-        stats[nation].extend(victory_points)
+                inner_html = int(ranking_points_element.get_attribute("innerText").strip())
+
+                victory_points.append(inner_html)
+                button = WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                "#func_day_down"))
+                )
+                button.click()
+
+                current_day_element = driver.find_element(By.CSS_SELECTOR, "#func_newspaper_day_tf")
+                current_day_value = current_day_element.get_attribute("value")
+
+                if current_day_value == "1":
+                    break
+
+            victory_points.reverse()
+            stats[nation].extend(victory_points)
+
+            driver.execute_script("document.elementFromPoint(1, 1).click();")
+
+            return_to_menu = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((
+                    By.CSS_SELECTOR,
+                    "#func_miniprofile_cont > div.actions.row > div.func_button_home.con_button.mini_profile_topbutton"
+                ))
+            )
+            return_to_menu.click()
+
+        count = len(elements)
+        for x in range(1, count + 1):
+            if x != 1:
+                driver.switch_to.default_content()
+                ifr = driver.find_element(By.ID, "ifm")
+                driver.switch_to.frame(ifr)
+
+                driver.execute_script("""
+                                       var popupContainer = document.getElementById('popupsContainer');
+                                       if (popupContainer) {
+                                           popupContainer.innerHTML = '';
+                                       }
+                                   """)
+
+            enter_game = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, f"#uber_my_games > div:nth-child({x}) > div > div"))
+            )
+            enter_game.click()
+            get_data()
+
+            time.sleep(3)  # TODO: not sure how many to be
+
         # print(stats)
         # time.sleep(100)
     finally:
