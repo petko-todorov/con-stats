7f30bdc5bde2fbf60ff3d09709167cdda3d62716
Petko Todorov <p.todorov1015@gmail.com>
2025-02-24 12:11:38 +0200

    add scrape kd stats

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scraper import scrape_data
 from data_handler import save_to_csv, load_from_csv
 from visualizer import visualizer
+from kd_stats import scrape_kd_stats, save_kd_stats_to_csv, visualize_kd_stats
 
 
 def scrape_and_save():
@@ -17,11 +18,19 @@ def load_and_visualize():
     visualizer(data)
 
 
+def scrape_kd_stats_and_visualize():
+    kd_stats = scrape_kd_stats()
+    save_kd_stats_to_csv(kd_stats)
+    visualize_kd_stats()
+
+
 if __name__ == '__main__':
     while True:
-        print("1. Scrape data and see graph")
-        print("2. See only graph")
-        print("3. Exit")
+        print("1. Scrape victory points per day data and see graph")
+        print("2. Show only the victory points graph")
+        print("3. Scrape kd stats and see graph")
+        print("4. Show only KD graph (The information may be outdated)")
+        print("5. Exit")
 
         try:
             choice = int(input("Your choice: "))
@@ -32,7 +41,9 @@ if __name__ == '__main__':
         options = {
             1: scrape_and_save,
             2: load_and_visualize,
-            3: exit
+            3: scrape_kd_stats_and_visualize,
+            4: visualize_kd_stats,
+            5: exit
         }
 
         options.get(choice, exit)()
